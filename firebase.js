e8d216d5ace59ac6636947ffd5f3a21e2d834b25
uesrheimes <uesrhedo@gmail.com>
2026-05-27 09:41:35 +0100

    Add Firebase initialization in firebase.js

---
+++ firebase.js
@@ -0,0 +1,22 @@
+import { initializeApp }
+from "https://www.gstatic.com/firebasejs/12.13.0/firebase-app.js";
+
+const firebaseConfig = {
+
+  apiKey: "AIzaSyBo4Bm4wqpfEvG58G5QVpt1TCbCvCCWFFM",
+
+  authDomain: "forex-chart-cd349.firebaseapp.com",
+
+  projectId: "forex-chart-cd349",
+
+  storageBucket: "forex-chart-cd349.firebasestorage.app",
+
+  messagingSenderId: "714964986603",
+
+  appId: "1:714964986603:web:9c7846b99a856c9ca86cae"
+
+};
+
+const app = initializeApp(firebaseConfig);
+
+export default app;
